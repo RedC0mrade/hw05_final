cfe0f2d72072e2096cacd33f5310b6ad6ce2d4d6
Бутенко Ниолай <e2e4e2e4@mail.ru>
2023-03-29 12:08:52 +0200

    fin2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -128,5 +128,6 @@ def profile_follow(request, username):
 
 @login_required
 def profile_unfollow(request, username):
-    Follow.objects.filter(author__username=username, user=request.user).delete()
+    Follow.objects.filter(
+        author__username=username, user=request.user).delete()
     return redirect('posts:profile', username=username)
