d546edd3cae97528a600f1462a276fe9da53e57e
Бутенко Ниолай <e2e4e2e4@mail.ru>
2023-03-29 12:06:59 +0200

    fin2

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
 
+def csrf_failure(request, reason=""):
+    return render(request, "core/403csrf.html")
+
+
 def page_not_found(request, exception):
     return render(request, "core/404.html", {"path": request.path}, status=404)
 
 
-def permission_denied(request, reason=""):
+def permission_denied(request, exception):
     return render(request, "core/403.html")
